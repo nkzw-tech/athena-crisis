5331ef7e1b90f4bb244497732248c997087a2114
Christoph Nakazawa <christoph.pojer@gmail.com>
2024-05-19 13:58:37 +0900

    Add `@typescript-eslint/array-type` eslint rule and Symmetrical Drawing Mode (#33)
    Co-authored-by: cpojer <christoph.pojer@gmail.com>
    Co-authored-by: Dominik Kratz <dominik.kratz94@gmail.com>
    GitOrigin-RevId: 124c9db050d5697a3cddfe85615f7eff541e6bea

--- hera/editor/Types.tsx
+++ hera/editor/Types.tsx
@@ -38,9 +38,16 @@ type UndoKey =
 
 export type UndoEntry = readonly [UndoKey, MapData];
 export type UndoStack = ReadonlyArray<UndoEntry>;
+export type DrawingMode =
+  | 'regular'
+  | 'horizontal'
+  | 'vertical'
+  | 'horizontal-vertical'
+  | 'diagonal';
 
 export type EditorState = Readonly<{
   condition?: readonly [WinConditionsWithVectors, number];
+  drawingMode: DrawingMode;
   effects: Effects;
   isDrawing: boolean;
   isErasing: boolean;
