8bbff37aea32c1693d7d0559b935f91c7b346b09
Drake Costa <drake@saeris.io>
2024-05-21 12:38:48 -0700

    actually fix quotes

--- ui/Browser.tsx
+++ ui/Browser.tsx
@@ -4,10 +4,10 @@ declare global {
 }
 
 const maybeWindow =
-  typeof window === "undefined"
+  typeof window === 'undefined'
     ? {
         HTMLElement: false,
-        navigator: { maxTouchPoints: 0, userAgent: "" },
+        navigator: { maxTouchPoints: 0, userAgent: '' },
         safari: null,
       }
     : window;
@@ -24,12 +24,12 @@ export const isIOS =
 export const isSafari =
   /constructor/i.test(maybeWindow.HTMLElement as unknown as string) ||
   ((pushNotification) =>
-    pushNotification.toString() === "[object SafariRemoteNotification]")(
-    !maybeWindow["safari"] ||
-      (typeof safari !== "undefined" && maybeWindow["safari"].pushNotification),
+    pushNotification.toString() === '[object SafariRemoteNotification]')(
+    !maybeWindow['safari'] ||
+      (typeof safari !== 'undefined' && maybeWindow['safari'].pushNotification),
   ) ||
   isIOS;
 
 export const isWindows = /Win(dows|32|64|NT)/.test(navigator.userAgent);
 
-export const isFirefox = navigator.userAgent.toLowerCase().includes("firefox");
+export const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
