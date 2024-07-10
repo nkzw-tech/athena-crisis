13c164f8ad3aef99dbddb15eaa9a92a408238c5c
cpojer <christoph.pojer@gmail.com>
2024-07-10 15:15:38 +0900

    Handle two controllers by default.
    GitOrigin-RevId: e89202241ba113d6bc35c6a69f448ba7420d903a

--- ui/controls/setupGamePad.tsx
+++ ui/controls/setupGamePad.tsx
@@ -3,6 +3,7 @@ import {
   createJoymap,
   createQueryModule,
   Joymap,
+  QueryModule,
 } from '@nkzw/joymap';
 import { getCurrentScrollContainer } from '../ScrollContainer.tsx';
 import dynamicThrottle from './dynamicThrottle.tsx';
@@ -13,7 +14,8 @@ const singlePress = (button?: ButtonResult) =>
   !!(button?.pressed && button.justChanged);
 
 const pressed = (button?: ButtonResult) => !!button?.pressed;
-const query = createQueryModule();
+const controller1 = createQueryModule();
+const controller2 = createQueryModule();
 let useSecondaryNavigation = false;
 let joymap: Joymap | null = null;
 
@@ -26,7 +28,6 @@ export default function setupGamePad() {
     return;
   }
 
-  let needsThrottleReset = false;
   const navigate = dynamicThrottle(
     (direction: { x: -1 | 0 | 1; y: -1 | 0 | 1 }) => {
       Input.fire(
@@ -39,8 +40,175 @@ export default function setupGamePad() {
 
   const next = throttle(() => Input.fire('next'), 250);
   const previous = throttle(() => Input.fire('previous'), 250);
+  let needsThrottleReset = false;
   let tertiaryPressed = false;
   let detailPressed = false;
+
+  const handleInputs = (controller: QueryModule) => {
+    const stickLeft = controller.getStick('L');
+    const {
+      A,
+      B,
+      L1,
+      L2,
+      L3,
+      R1,
+      R2,
+      R3,
+      X,
+      Y,
+      dpadDown,
+      dpadLeft,
+      dpadRight,
+      dpadUp,
+      home,
+      select,
+      start,
+    } = controller.getAllButtons();
+    if (needsThrottleReset) {
+      if (
+        stickLeft &&
+        !stickLeft.pressed &&
+        dpadUp &&
+        !dpadUp.pressed &&
+        dpadRight &&
+        !dpadRight.pressed &&
+        dpadDown &&
+        !dpadDown.pressed &&
+        dpadLeft &&
+        !dpadLeft.pressed
+      ) {
+        needsThrottleReset = false;
+        navigate.reset();
+      }
+    }
+
+    const direction: NavigationDirection = { x: 0, y: 0 };
+    if (stickLeft?.type === 'stick' && stickLeft.pressed) {
+      const [x, y] = stickLeft.value;
+      const deltaX = Math.abs(x);
+      const deltaY = Math.abs(y);
+      direction.x = deltaX > 0.4 ? (Math.sign(x) as 1 | -1) : 0;
+      direction.y = deltaY > 0.4 ? (Math.sign(y) as 1 | -1) : 0;
+      if (direction.x && !direction.y && deltaX < 0.75 && deltaY > 0.25) {
+        direction.y = Math.sign(y) as 1 | -1;
+      } else if (
+        direction.y &&
+        !direction.x &&
+        deltaY < 0.75 &&
+        deltaX > 0.25
+      ) {
+        direction.x = Math.sign(x) as 1 | -1;
+      }
+    } else {
+      if (dpadUp?.pressed) {
+        direction.y -= 1;
+      }
+      if (dpadRight?.pressed) {
+        direction.x += 1;
+      }
+      if (dpadDown?.pressed) {
+        direction.y += 1;
+      }
+      if (dpadLeft?.pressed) {
+        direction.x -= 1;
+      }
+    }
+
+    if (direction.x !== 0 || direction.y !== 0) {
+      needsThrottleReset = true;
+      navigate(direction);
+    }
+
+    const stickRight = controller.getStick('R');
+    if (stickRight?.type === 'stick' && stickRight.pressed) {
+      getCurrentScrollContainer().scrollBy(
+        stickRight.value[0] * 16,
+        stickRight.value[1] * 16,
+      );
+      Input.fire('point');
+    }
+
+    if (singlePress(start) || singlePress(home)) {
+      Input.fire('menu');
+    }
+
+    if (singlePress(A)) {
+      navigate.reset();
+      Input.fire('accept');
+    }
+
+    if (singlePress(select)) {
+      Input.fire('select', { modifier: A?.pressed });
+    }
+
+    if (singlePress(B)) {
+      navigate.reset();
+      Input.fire('cancel', null);
+    }
+
+    if (singlePress(X)) {
+      Input.fire('secondary');
+    }
+
+    if (singlePress(Y)) {
+      tertiaryPressed = true;
+      Input.fire('tertiary');
+      Input.fire('gamepad-tertiary');
+    }
+
+    if (tertiaryPressed && !pressed(Y)) {
+      tertiaryPressed = false;
+      Input.fire('tertiary:released');
+    }
+
+    const l1Pressed = pressed(L1);
+    const r1Pressed = pressed(R1);
+    if (l1Pressed && r1Pressed) {
+      Input.fire('undo');
+    } else {
+      if (l1Pressed) {
+        previous();
+      }
+
+      if (r1Pressed) {
+        next();
+      }
+    }
+
+    const l2pressed = pressed(L2);
+    const r2pressed = pressed(R2);
+    if (l2pressed && r2pressed) {
+      useSecondaryNavigation = true;
+    } else {
+      useSecondaryNavigation = false;
+      if (singlePress(L2)) {
+        Input.fire('info');
+      }
+
+      if (singlePress(R2)) {
+        detailPressed = true;
+        Input.fire('detail');
+      }
+
+      if (detailPressed && !r2pressed) {
+        detailPressed = false;
+        Input.fire('detail:released');
+      }
+    }
+
+    if (singlePress(R3)) {
+      Input.fire('zoom');
+    }
+
+    if (singlePress(L3)) {
+      Input.fire('quaternary');
+    }
+
+    if (pressed(B) && pressed(A) && l1Pressed && r1Pressed) {
+      Input.fire('reset');
+    }
+  };
 
   joymap = createJoymap({
     autoConnect: true,
@@ -54,173 +222,15 @@ export default function setupGamePad() {
         return;
       }
 
-      const stickLeft = query.getStick('L');
-      const {
-        A,
-        B,
-        L1,
-        L2,
-        L3,
-        R1,
-        R2,
-        R3,
-        X,
-        Y,
-        dpadDown,
-        dpadLeft,
-        dpadRight,
-        dpadUp,
-        home,
-        select,
-        start,
-      } = query.getAllButtons();
-      if (needsThrottleReset) {
-        if (
-          stickLeft &&
-          !stickLeft.pressed &&
-          dpadUp &&
-          !dpadUp.pressed &&
-          dpadRight &&
-          !dpadRight.pressed &&
-          dpadDown &&
-          !dpadDown.pressed &&
-          dpadLeft &&
-          !dpadLeft.pressed
-        ) {
-          needsThrottleReset = false;
-          navigate.reset();
-        }
-      }
-
-      const direction: NavigationDirection = { x: 0, y: 0 };
-      if (stickLeft?.type === 'stick' && stickLeft.pressed) {
-        const [x, y] = stickLeft.value;
-        const deltaX = Math.abs(x);
-        const deltaY = Math.abs(y);
-        direction.x = deltaX > 0.4 ? (Math.sign(x) as 1 | -1) : 0;
-        direction.y = deltaY > 0.4 ? (Math.sign(y) as 1 | -1) : 0;
-        if (direction.x && !direction.y && deltaX < 0.75 && deltaY > 0.25) {
-          direction.y = Math.sign(y) as 1 | -1;
-        } else if (
-          direction.y &&
-          !direction.x &&
-          deltaY < 0.75 &&
-          deltaX > 0.25
-        ) {
-          direction.x = Math.sign(x) as 1 | -1;
-        }
-      } else {
-        if (dpadUp?.pressed) {
-          direction.y -= 1;
-        }
-        if (dpadRight?.pressed) {
-          direction.x += 1;
-        }
-        if (dpadDown?.pressed) {
-          direction.y += 1;
-        }
-        if (dpadLeft?.pressed) {
-          direction.x -= 1;
-        }
-      }
-
-      if (direction.x !== 0 || direction.y !== 0) {
-        needsThrottleReset = true;
-        navigate(direction);
-      }
-
-      const stickRight = query.getStick('R');
-      if (stickRight?.type === 'stick' && stickRight.pressed) {
-        getCurrentScrollContainer().scrollBy(
-          stickRight.value[0] * 16,
-          stickRight.value[1] * 16,
-        );
-        Input.fire('point');
-      }
-
-      if (singlePress(start) || singlePress(home)) {
-        Input.fire('menu');
-      }
-
-      if (singlePress(A)) {
-        navigate.reset();
-        Input.fire('accept');
-      }
-
-      if (singlePress(select)) {
-        Input.fire('select', { modifier: A?.pressed });
-      }
-
-      if (singlePress(B)) {
-        navigate.reset();
-        Input.fire('cancel', null);
-      }
-
-      if (singlePress(X)) {
-        Input.fire('secondary');
-      }
-
-      if (singlePress(Y)) {
-        tertiaryPressed = true;
-        Input.fire('tertiary');
-        Input.fire('gamepad-tertiary');
-      }
-
-      if (tertiaryPressed && !pressed(Y)) {
-        tertiaryPressed = false;
-        Input.fire('tertiary:released');
-      }
-
-      const l1Pressed = pressed(L1);
-      const r1Pressed = pressed(R1);
-      if (l1Pressed && r1Pressed) {
-        Input.fire('undo');
-      } else {
-        if (l1Pressed) {
-          previous();
-        }
-
-        if (r1Pressed) {
-          next();
-        }
-      }
-
-      const l2pressed = pressed(L2);
-      const r2pressed = pressed(R2);
-      if (l2pressed && r2pressed) {
-        useSecondaryNavigation = true;
-      } else {
-        useSecondaryNavigation = false;
-        if (singlePress(L2)) {
-          Input.fire('info');
-        }
-
-        if (singlePress(R2)) {
-          detailPressed = true;
-          Input.fire('detail');
-        }
-
-        if (detailPressed && !r2pressed) {
-          detailPressed = false;
-          Input.fire('detail:released');
-        }
-      }
-
-      if (singlePress(R3)) {
-        Input.fire('zoom');
-      }
-
-      if (singlePress(L3)) {
-        Input.fire('quaternary');
-      }
-
-      if (pressed(B) && pressed(A) && l1Pressed && r1Pressed) {
-        Input.fire('reset');
+      handleInputs(controller1);
+      if (gamepads.length > 1) {
+        handleInputs(controller2);
       }
     },
   });
 
-  joymap.addModule(query);
+  joymap.addModule(controller1);
+  joymap.addModule(controller2);
   try {
     joymap.start();
   } catch {
@@ -237,7 +247,7 @@ export default function setupGamePad() {
 export type Rumble = 'explosion' | 'menu' | 'accept';
 
 export function rumbleEffect(type: Rumble, duration?: number) {
-  query.addRumble(
+  const effect =
     type === 'explosion'
       ? {
           duration: duration || 200,
@@ -254,7 +264,8 @@ export function rumbleEffect(type: Rumble, duration?: number) {
             duration: duration || 50,
             strongMagnitude: 0.3,
             weakMagnitude: 0.3,
-          },
-    type,
-  );
+          };
+
+  controller1.addRumble(effect, type);
+  controller2.addRumble(effect, type);
 }
