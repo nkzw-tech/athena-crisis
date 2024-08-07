34c7470194b438290ed9d083d83ee05df5d2cea2
cpojer <christoph.pojer@gmail.com>
2024-08-07 18:35:24 +0900

    Fix a Map Editor crash.
    GitOrigin-RevId: 0b48e3ff0ca7dbd33ff15a1404b9ff406df64678

--- athena/lib/isPvP.tsx
+++ athena/lib/isPvP.tsx
@@ -2,6 +2,7 @@ import MapData from '../MapData.tsx';
 
 export default function isPvP(map: MapData) {
   return (
-    map.active.filter((id) => map.getPlayer(id).isHumanPlayer()).length > 1
+    map.active.filter((id) => map.maybeGetPlayer(id)?.isHumanPlayer()).length >
+    1
   );
 }
