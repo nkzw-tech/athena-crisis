d9516afa42d4154e504912a69906e21d7b07b29d
cpojer <christoph.pojer@gmail.com>
2024-09-24 22:40:01 +0900

    Invasion improvements.
    GitOrigin-RevId: 33eb2e03969fee804159c3db232b2f0b73cf91b3

--- athena/lib/calculateEmptyClusters.tsx
+++ athena/lib/calculateEmptyClusters.tsx
@@ -5,11 +5,11 @@ export default function calculateEmptyClusters(map: MapData) {
   const movementTypes = [
     ...new Set(map.units.map((unit) => unit.info.movementType).values()),
   ];
-
   const occupied = new Set(
-    [...map.units.keys(), ...map.buildings.keys()].flatMap((vector) =>
-      vector.expandWithDiagonals(),
-    ),
+    [
+      ...map.units.keys(),
+      ...map.buildings.filter((building) => building.info.isHQ()).keys(),
+    ].flatMap((vector) => vector.expandWithDiagonals()),
   );
   return calculateClusters(
     map.size,
