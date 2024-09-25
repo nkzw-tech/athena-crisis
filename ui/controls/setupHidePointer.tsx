256638ec6149a6b9647fe7d0478b78c6606d1e0a
cpojer <christoph.pojer@gmail.com>
2024-09-25 15:36:59 +0900

    Disable pointer lock in Firefox.
    GitOrigin-RevId: 29d0ad6727e66f368feaf05ec3a1ddb89b2a474a

--- ui/controls/setupHidePointer.tsx
+++ ui/controls/setupHidePointer.tsx
@@ -1,3 +1,5 @@
+import { isFirefox } from '../Browser.tsx';
+
 let pointerEnabled = true;
 
 const showPointer = () => {
@@ -25,7 +27,7 @@ export function hidePointer() {
 let setup = false;
 
 export default function setupHidePointer() {
-  if (setup) {
+  if (setup || isFirefox) {
     return;
   }
 
