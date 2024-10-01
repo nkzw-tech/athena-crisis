034dbc0f561ea5418cb130bdfb2a280ab0977c81
cpojer <christoph.pojer@gmail.com>
2024-10-01 15:06:32 +0900

    Add `window` to workers 🫠
    GitOrigin-RevId: 3d58190f1de59d86b850614e05b89b3b777aa1f3

--- hera/workers/initializeWorker.tsx
+++ hera/workers/initializeWorker.tsx
@@ -6,6 +6,7 @@ declare global {
 }
 
 if (import.meta.hot) {
+  self.window = self;
   self.$RefreshReg$ = () => {};
   // eslint-disable-next-line unicorn/consistent-function-scoping
   self.$RefreshSig$ = () => (type) => type;
