df1ee2c69499d107d4c0af8ba8585270f8996073
cpojer <christoph.pojer@gmail.com>
2024-10-01 22:08:35 +0900

    Show the campaign stars per player when replaying campaign maps.
    GitOrigin-RevId: d44b9488242e500c4748cf18f48df3fa2cc41abe

---
+++ hera/hooks/usePlayerAchievement.tsx
@@ -0,0 +1,24 @@
+import {
+  maybeDecodePlayerPerformance,
+  PerformanceType,
+  PlayerPerformance,
+} from '@deities/athena/map/PlayerPerformance.tsx';
+import { useMemo } from 'react';
+
+export default function usePlayerAchievement(
+  achievementResult: string,
+): [playerPerformance: PlayerPerformance | null, count: string] {
+  return useMemo(() => {
+    const result = maybeDecodePlayerPerformance(achievementResult);
+    return [
+      result,
+      result
+        ? String(
+            Object.keys(result).filter(
+              (type) => result[type as PerformanceType] != null,
+            ).length,
+          )
+        : '?',
+    ] as const;
+  }, [achievementResult]);
+}
