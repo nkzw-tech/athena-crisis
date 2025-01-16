a7149eca6caabf18308131828f3d52e52ba91e20
cpojer <christoph.pojer@gmail.com>
2025-01-16 11:26:28 +0900

    Cleanup.
    GitOrigin-RevId: 34c8185dad10218e8239d6f47132d666ccbc38c0

--- prettier.config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-  importOrderParserPlugins: ['importAssertions', 'typescript', 'jsx'],
-  plugins: [
-    '@ianvs/prettier-plugin-sort-imports',
-    'prettier-plugin-packagejson',
-  ],
-  singleQuote: true,
-};
