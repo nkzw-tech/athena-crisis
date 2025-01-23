99faec5a19c2f41b5ac41fb5917acf50094b5bec
cpojer <christoph.pojer@gmail.com>
2025-01-24 07:21:33 +0900

    Fix open source CI.
    GitOrigin-RevId: a73baed8ed5e8c26b2e3bfc08b318a6ead1f5ab5

--- infra/babelPresets.tsx
+++ infra/babelPresets.tsx
@@ -1,10 +1,13 @@
+import { join } from 'node:path';
 import fbteePreset from '@nkzw/babel-preset-fbtee';
 import fbtCommon from '../i18n/Common.ts';
 import isOpenSource from './isOpenSource.tsx';
 
+const manifestFile = join(process.cwd(), './ares/.enum_manifest.json');
+
 const getEnumManifest = async () => {
   try {
-    return await import('../ares/.enum_manifest.json');
+    return await import(manifestFile);
   } catch {
     if (!isOpenSource()) {
       throw new Error('babelPresets: Missing enum manifest.');
