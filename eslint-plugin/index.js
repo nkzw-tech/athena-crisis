4e9dfbe15fa572a164c913af4afd021dc658a0af
cpojer <christoph.pojer@gmail.com>
2025-04-02 12:51:45 +0900

    Update deps.
    GitOrigin-RevId: 88660de86ebc5b63af8106a580658ebc44bf3d2d

--- eslint-plugin/index.js
+++ eslint-plugin/index.js
@@ -2,8 +2,6 @@ import noCopyExpression from './no-copy-expression.js';
 import noDateNow from './no-date-now.js';
 import noInlineCSS from './no-inline-css.js';
 import noLazyImport from './no-lazy-import.js';
-import requireUseEffectArguments from './require-use-effect-arguments.js';
-import useRelayTypes from './use-relay-types.js';
 
 export default {
   configs: {
@@ -13,8 +11,6 @@ export default {
         '@deities/no-date-now': 2,
         '@deities/no-inline-css': 2,
         '@deities/no-lazy-import': 2,
-        '@deities/require-use-effect-arguments': 2,
-        '@deities/use-relay-types': 2,
       },
     },
   },
@@ -26,7 +22,5 @@ export default {
     'no-date-now': noDateNow,
     'no-inline-css': noInlineCSS,
     'no-lazy-import': noLazyImport,
-    'require-use-effect-arguments': requireUseEffectArguments,
-    'use-relay-types': useRelayTypes,
   },
 };
