8a3a1c86151190e6b6529c2cc58f2eaf9f72b2c8
cpojer <christoph.pojer@gmail.com>
2025-05-26 11:04:45 +0900

    Fix local time drifting based on server time.
    GitOrigin-RevId: 1b6e32132c959df102712a6bb5ccfa24fd7a0f30

--- apollo/lib/dateNow.tsx
+++ apollo/lib/dateNow.tsx
@@ -4,7 +4,8 @@ export const ONE_HOUR = 1000 * 60 * 60;
 export const ONE_DAY = ONE_HOUR * 24;
 
 export function setTime(serverTime: number) {
-  timeDifference = serverTime - dateNow();
+  // eslint-disable-next-line @deities/no-date-now
+  timeDifference = serverTime - Date.now();
 }
 
 export default function dateNow() {
