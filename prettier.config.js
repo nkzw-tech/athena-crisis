a51f631c7a03de537be1265b20953af7fe8817c3
cpojer <christoph.pojer@gmail.com>
2025-06-24 09:13:08 +0900

    Update deps.
    GitOrigin-RevId: 05b90c8fc987c6311e7617b9e19cefb3385b4003

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 export default {
   importOrderParserPlugins: ['importAssertions', 'typescript', 'jsx'],
   plugins: [
+    '@prettier/plugin-oxc',
     '@ianvs/prettier-plugin-sort-imports',
     'prettier-plugin-packagejson',
   ],
