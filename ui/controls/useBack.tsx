22bcdbd45c4d0f134bab57b974942d21092484be
cpojer <christoph.pojer@gmail.com>
2026-05-13 23:45:48 +0900

    Make the back navigation behavior more consistent.
    GitOrigin-RevId: 97bac0de7f59c5bd06c6c213e17a75f4a54b5c81

--- ui/controls/useBack.tsx
+++ ui/controls/useBack.tsx
@@ -1,13 +1,18 @@
 import { Route } from '@deities/apollo/Routes.tsx';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import useLocation from '../hooks/useLocation.tsx';
 import useNavigate from '../hooks/useNavigate.tsx';
 import useInput from './useInput.tsx';
 
+export function getBackURL(search: string): Route | null {
+  const backURL = new URLSearchParams(search).get('back') as Route | null;
+  return backURL && backURL !== '/' && !backURL.startsWith('/?') ? backURL : null;
+}
+
 export default function useBack(to: Route) {
   const { search } = useLocation();
   const navigate = useNavigate();
-  const backURL = (new URLSearchParams(search).get('back') as Route | undefined) || null;
+  const backURL = useMemo(() => getBackURL(search), [search]);
 
   useInput(
     'cancel',
