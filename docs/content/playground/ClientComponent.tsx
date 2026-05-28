b39139246d9ec9358b8057a74080b8d904edd1e5
jxom <7336481+jxom@users.noreply.github.com>
2026-05-29 03:51:06 +1000

    docs: update vocs
    GitOrigin-RevId: 4ecfc7353a79312cb3de999cce9e5942852f76b6

--- docs/content/playground/ClientComponent.tsx
+++ docs/content/playground/ClientComponent.tsx
@@ -1,9 +1,20 @@
+'use client';
+
 import Spinner from '@deities/ui/Spinner.tsx';
 import Stack from '@nkzw/stack';
-import { ReactElement, Suspense, useEffect, useState } from 'react';
+import { lazy, ReactElement, Suspense, useEffect, useState } from 'react';
 
-export default function ClientComponent({ module: Module }: { module: () => ReactElement }) {
+const modules = {
+  entities: lazy(() => import('../examples/entities-example.tsx')),
+  mapData: lazy(() => import('../examples/map-data-examples.tsx')),
+  mapEditor: lazy(() => import('../examples/map-editor.tsx')),
+  playgroundDemoGame: lazy(() => import('./PlaygroundDemoGame.tsx')),
+  portraits: lazy(() => import('../examples/portraits-example.tsx')),
+};
+
+export default function ClientComponent({ module }: { module: keyof typeof modules }) {
   const [element, setElement] = useState<ReactElement | null>(null);
+  const Module = modules[module];
 
   useEffect(() => {
     import('./ClientScope.tsx').then(({ default: ClientScope }) =>
