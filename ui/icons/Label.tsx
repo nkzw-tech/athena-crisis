a0f4f0df52269d6305e461120c17fcf205e1fc1a
cpojer <christoph.pojer@gmail.com>
2026-06-03 14:26:59 +0900

    Fix label color.
    GitOrigin-RevId: 1ec90531679543d4e67fe7a4805b171d51e07137

--- ui/icons/Label.tsx
+++ ui/icons/Label.tsx
@@ -1,5 +1,5 @@
 export default {
-  body: `<path d="M12 4h2v2h2v2h2v2h2v2h2v4h-2v2h-2v2h-2v2h-4v-2h-2v-2H8v-2H6v-2H4v-2H2V4h2V2h8v2ZM6 8h2V6H6v2Z"></path>`,
+  body: `<path fill="currentColor" d="M12 4h2v2h2v2h2v2h2v2h2v4h-2v2h-2v2h-2v2h-4v-2h-2v-2H8v-2H6v-2H4v-2H2V4h2V2h8v2ZM6 8h2V6H6v2Z"></path>`,
   height: 24,
   width: 24,
 };
