a266850834ab26bc8ac975493f16635e25529d6c
cpojer <christoph.pojer@gmail.com>
2026-09-04 22:18:04 +0900

    Fix "More" icon for buying units and skills.
    GitOrigin-RevId: 9ee1eba299cafd6c81993dfa4602b127a3ad1ff4

---
+++ ui/icons/MoreHorizontal.tsx
@@ -0,0 +1,5 @@
+export default {
+  body: `<path fill="currentColor" fill-rule="evenodd" d="M1 9h6v6H1zm2 2v2h2v-2zm6-2h6v6H9zm2 2v2h2v-2zm6-2h6v6h-6zm2 2v2h2v-2z" clip-rule="evenodd"/>`,
+  height: 24,
+  width: 24,
+};
